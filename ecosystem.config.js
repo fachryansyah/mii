ea3a8ab827a0995128165be97570acc7ccac0e56
Muhammad Fahriansyah <fachryansyah123@gmail.com>
2024-01-13 16:23:20 +0700

    adding ecosystem for deployment

---
+++ ecosystem.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  apps: [
+    {
+      name: "mii",
+      script: "serve dist/ src -n -p 6000", // Path to the compiled main.js file
+      instances: 1, // Number of instances to run
+      autorestart: true, // Automatically restart the app if it crashes
+      watch: false, // Set to true to enable file watching and automatic restart on file changes
+      env: {
+        NODE_ENV: "production",
+      },
+    },
+  ],
+};
