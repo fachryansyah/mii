490e9ae59e33e090d5fc60544abec24e9e70dd7e
Muhammad Fahriansyah <fachryansyah123@gmail.com>
2024-01-13 16:55:36 +0700

    update

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: "mii",
-      script: "serve dist -n -p 7000", // Path to the compiled main.js file
+      script: "serve dist -n -p 3003", // Path to the compiled main.js file
       instances: 1, // Number of instances to run
       autorestart: true, // Automatically restart the app if it crashes
       watch: false, // Set to true to enable file watching and automatic restart on file changes
