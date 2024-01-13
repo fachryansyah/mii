d04ba1248691e495ef37c27bf3f3954f3b661557
Muhammad Fahriansyah <fachryansyah123@gmail.com>
2024-01-13 16:28:27 +0700

    update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,14 +1,12 @@
 module.exports = {
-  apps: [
-    {
-      name: "mii",
-      script: "serve dist/ src -n -p 6000", // Path to the compiled main.js file
-      instances: 1, // Number of instances to run
-      autorestart: true, // Automatically restart the app if it crashes
-      watch: false, // Set to true to enable file watching and automatic restart on file changes
-      env: {
-        NODE_ENV: "production",
-      },
+  apps : [{
+    name: 'mii',
+    script: 'serve dist/ src -n -p 6000',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    env: {
+      NODE_ENV: 'production',
     },
-  ],
-};
+  },]
+}
